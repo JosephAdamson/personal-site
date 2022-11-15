d7c8701c814789cc1f6a4c7ca5bd06bdf18f3ead
Joseph Adamson <josephowenadamson@gmail.com>
2022-11-15 16:15:36 +0000

    trying to get netlify form to register

--- src/components/ContactInsert.js
+++ src/components/ContactInsert.js
@@ -2,33 +2,38 @@
 
 function ContactInsert() {
     return (
-        <form 
-        className="flex flex-col items-center justify-center md:h-full" 
-        method="POST"
-        name="contact"
-        onSubmit="submit"
-        netlify>
-            <div className="flex flex-col justify-between w-2/3">
-                <label>name</label>
-                <input type="text" name="sender-name"/>
+        <div className="flex flex-col">
+            <div className="flex justify-center my-2">
+                <h1>Drop me a message!</h1>
             </div>
-            <br/>
-            <div className="flex flex-col justify-between w-2/3">
-                <label>email</label>
-                <input type="text" name="sender-email"/>
-            </div>
-            <br/>
-            <div className="flex flex-col justify-between w-2/3 h-40">
-                <label>comment</label>
-                <textarea className="h-full" name="sender-comment"/>
-            </div>
-            <div className="flex w-2/3 ustify-start my-4">
-                <input className="p-1"type="submit"/>
-            </div>
-            {/* connect netlify form*/}
-            <input type="hidden" name="form-name" value="contact" />
-        </form>
-    )
+            <form 
+            className="flex flex-col items-center justify-center md:h-full" 
+            method="POST"
+            name="contact"
+            onSubmit="submit"
+            netlify>
+                <div className="flex flex-col justify-between w-2/3">
+                    <label>name</label>
+                    <input type="text" name="sender-name"/>
+                </div>
+                <br/>
+                <div className="flex flex-col justify-between w-2/3">
+                    <label>email</label>
+                    <input type="text" name="sender-email"/>
+                </div>
+                <br/>
+                <div className="flex flex-col justify-between w-2/3 h-40">
+                    <label>comment</label>
+                    <textarea className="h-full" name="sender-comment"/>
+                </div>
+                <div className="flex w-2/3 ustify-start my-4">
+                    <input className="p-1"type="submit"/>
+                </div>
+                {/* connect netlify form*/}
+                <input type="hidden" name="form-name" value="contact" />
+            </form>
+        </div>
+    );
 }
 
 export default ContactInsert;
